ab0fe34c0b2b8d2aee264b86cd210de5c8b72585
Dan-Hodges <dhdgs690@gmail.com>
2015-07-29 18:50:19 -0500

    added language translation feature and updated index structure

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 var translator = (function() {
-  var inputArray = [], inputString, translatedString, outputSelected;
+  var inputArray = [], inputString, translatedString, hungarianArray = [], hungarianString, outputSelected;
 
   $('#submitBtn').click(function() {
-    inputString = $("#translateField").val()
+    inputString = $("#translateField").val();
+    inputString = inputString.toLowerCase();
     inputArray = inputString.split(" ");
-    console.log(inputArray);
+    console.log("inputArray", inputArray);
     outputSelected = $('option:selected').attr('id');
-    console.log(outputSelected);
+
+    for (var i = 0; i < inputArray.length; i++) {
+      if (inputArray[i] === "have") {
+        hungarianArray.push("van");
+      }
+      if (inputArray[i] === "a") {
+        hungarianArray.push("egy");
+      }
+      if (inputArray[i] === "merry") {
+        hungarianArray.push("vidám");
+      }
+      if (inputArray[i] === "christmas") {
+        hungarianArray.push("Karácsony");
+      }
+      if (inputArray[i] === "and") {
+        hungarianArray.push("és");
+      }
+      if (inputArray[i] === "happy") {
+        hungarianArray.push("boldog");
+      }
+      if (inputArray[i] === "new") {
+        hungarianArray.push("új");
+      }
+      if (inputArray[i] === "year") {
+        hungarianArray.push("év");
+      }
+    };
+
+    hungarianString = hungarianArray.join(" ");
+    console.log(hungarianString);
+    $("#translation").text(hungarianString); 
   });
 
   return {
@@ -30,6 +61,3 @@ var translator = (function() {
   };
 
 })();
-
-
-
